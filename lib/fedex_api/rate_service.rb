8282848e75bc6440605624bcb42ea70598aaf13c
dedekm <dedekmm@gmail.com>
2019-08-08 14:34:08 +0200

    update rate service

--- lib/fedex_api/rate_service.rb
+++ lib/fedex_api/rate_service.rb
@@ -4,8 +4,15 @@ module FedexApi
   class RateService < BaseService
     WSDL_FILENAME = 'RateService_v24.wsdl'
 
+    attr_accessor :shipper, :recipient
+
     def initialize
       super(WSDL_FILENAME)
+
+      @packages = []
+
+      # defaults
+      @weight_units = 'KG'
     end
 
     def version
@@ -17,73 +24,58 @@ module FedexApi
       }
     end
 
-    def get_rates
+    def add_package(hash)
+      @packages << hash
+
+      hash
+    end
+
+    def get_rates(options = {})
       options = {
         return_transit_and_commit: true,
         requested_shipment: {
           ship_timestamp: Time.now.iso8601,
           dropoff_type: 'REGULAR_PICKUP',
           packaging_type: 'YOUR_PACKAGING',
-          total_weight: {
-            units: 'LB',
-            value: '20.0'
-          },
-          shipper: {
-            contact: {
-              company_name: 'test',
-              phone_number: '12345678'
-            },
-            address: {
-                 street_lines: [ 'address 1', 'address 2' ],
-                 city: 'Austin',
-                 state_or_province_code: 'TX',
-                 postal_code: '73301',
-                 country_code: 'US'
-            }
-          },
-          recipient: {
-            contact: {
-              company_name: 'test2',
-              phone_number: '98765432'
-            },
-            address: {
-                 street_lines: 'address',
-                 city: 'Collierville',
-                 state_or_province_code: 'TN',
-                 postal_code: '38017',
-                 country_code: 'US'
-            }
-          },
+          total_weight: total_weight,
+          shipper: shipper,
+          recipient: recipient,
           shipping_charges_payment: {
-            payment_type: 'SENDER'
+            payment_type: 'SENDER',
+            payor: {
+              responsible_party: {
+                account_number: ENV['FEDEX_ACCOUNT_NUMBER']
+              }
+            }
           },
           rate_request_types: 'LIST',
-          package_count: 1,
-          requested_package_line_items: {
-            sequence_number: 1,
-            group_number: 1,
-            group_package_count: 1,
-            weight: {
-              units: 'LB',
-              value: '20.0'
-            },
-            dimensions: {
-              length: 12,
-              width: 12,
-              height: 12,
-              units: 'IN'
-            },
-            content_records: {
-              part_number: 13234,
-              item_number: 'xyz123',
-              received_quantity: 12,
-              description: 'test'
-            }
-          }
+          package_count: @packages.count,
+          requested_package_line_items: requested_package_line_items
         }
-      }
+      }.merge(options)
 
       call(:get_rates, options)
     end
+
+    private
+      def total_weight
+        {
+          units: @weight_units,
+          value: @packages.sum { |p| p[:weight] }
+        }
+      end
+
+      def requested_package_line_items
+        @packages.map.with_index(1) do |package, i|
+          {
+            sequence_number: i,
+            group_package_count: 1,
+            weight: {
+              units: @weight_units,
+              value: package[:weight].to_s
+            }
+          }
+        end
+      end
   end
 end
